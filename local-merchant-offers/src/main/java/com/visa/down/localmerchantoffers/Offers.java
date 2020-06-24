ef0ffc40fdc933e3d8bf3cb5c16d225bfa529a56
KimchiBean <31521344+KimchiBean@users.noreply.github.com>
2020-06-24 17:15:02 -0500

    Simplified model
    Offers class made Offer obsolete

--- local-merchant-offers/src/main/java/com/visa/down/localmerchantoffers/Offers.java
+++ local-merchant-offers/src/main/java/com/visa/down/localmerchantoffers/Offers.java
@@ -5,21 +5,36 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Offers {
 
+    private String offerId;
     private String offerTitle;
+    private String activeIndicator;
+    private String validityFromDateTime;
+    private String validityToDateTime;
 
     public Offers() {
     }
 
-    public String getOfferTitle() {
-        return offerTitle;
-    }
+    public String getOfferId() { return offerId; }
+    public void setOfferId(String offerId) { this.offerId = offerId; }
 
-    public void setOfferTitle(String offerTitle) {
-        this.offerTitle = offerTitle;
-    }
+    public String getOfferTitle() { return offerTitle; }
+    public void setOfferTitle(String offerTitle) { this.offerTitle = offerTitle; }
+
+    public String getActiveIndicator() { return activeIndicator; }
+    public void setActiveStatus(String activeIndicator) { this.activeIndicator = activeIndicator; }
+
+    public String getValidityFromDateTime() { return validityFromDateTime; }
+    public void setValidityFromDateTime(String validityFromDateTime) { this.validityFromDateTime = validityFromDateTime; }
+
+    public String getValidityToDateTime() { return validityToDateTime; }
+    public void setValidityToDateTime(String validityToDateTime) { this.validityToDateTime = validityToDateTime; }
 
     @Override
     public String toString() {
-        return "{\"offerTitle\": \"" + offerTitle + "\"}";
+        return "{\"offerId\": \"" + offerId + "\","
+                + "\"offerTitle\": \"" + offerTitle + "\","
+                + "\"active\": \"" + activeIndicator + "\","
+                + "\"validityFrom\": \"" + validityFromDateTime + "\","
+                + "\"validityTo\": \"" + validityToDateTime + "\"}";
     }
 }
