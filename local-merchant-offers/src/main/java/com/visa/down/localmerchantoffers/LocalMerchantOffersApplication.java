17e2828b0650480562f35813d337911e8646346f
Alaina Lim <alainalim@Alainas-MBP.fios-router.home>
2020-06-24 13:18:03 -0400

    webb app

---
+++ local-merchant-offers/src/main/java/com/visa/down/localmerchantoffers/LocalMerchantOffersApplication.java
@@ -0,0 +1,13 @@
+package com.visa.down.localmerchantoffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalMerchantOffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalMerchantOffersApplication.class, args);
+	}
+
+}
