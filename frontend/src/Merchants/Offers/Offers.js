d21d9ffb5ef3bf5810a3df751bf3781f2467639a
Angela Wu <angela.wu@berkeley.edu>
2020-06-26 21:01:18 -0700

    tryna pull new patch

--- frontend/src/Merchants/Offers/Offers.js
+++ frontend/src/Merchants/Offers/Offers.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
+
 class Offers extends React.Component {
 
 	constructor(props) {
 		super(props);
 	}
 
+
 	render() {
 		return (
 			<ReactModal className="offers-background" isOpen={this.props.isVisible}>
 				<button onClick={this.props.closeModal}>Close Modal</button>
+				<div className="white-rect">
+					<p> asdfasdfasdfasdf </p>
+				</div>
 			</ReactModal>
 		);
 	}
