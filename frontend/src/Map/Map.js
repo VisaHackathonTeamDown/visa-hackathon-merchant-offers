81d4c712aff31c41c74d876776e078a88ef92228
Alaina Lim <alainalim@Alainas-MBP.fios-router.home>
2020-06-28 16:12:24 -0400

    working on search bar

--- frontend/src/Map/Map.js
+++ frontend/src/Map/Map.js
@@ -17,21 +17,65 @@ const mapStyles = {
   height: '45vh',
 };
 
+
+
+
+
 export class MapContainer extends Component {
+  state = {
+    location: ""
+  };
+
+  componentDidMount() {
+    const app = this;
+    function initAutocomplete() {
+      var input = document.getElementById("pac-input");
+      var searchBox = new window.google.maps.places.SearchBox(input);
+      searchBox.addListener("places_changed", function() {
+        app.setState({ location: document.getElementById("pac-input").value });
+      });
+    }
+    initAutocomplete();
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stores: [
+              { lat: 38.892038, lng: -77.198847 },
+              { lat: 38.492837, lng: -76.894038 }
+              ]
+    }
+  }
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return <Marker key ={index} id ={index} position ={{lat: store.lat, lng: store.lng}}
+      />
+    }
+
+    )
+  }
+
+  handleChange = e => {
+    this.setState({ location: e.target.value });
+  };
+
   render() {
     return (
       <div className="map-container">
+
         <Map
           google = {this.props.google}
           zoom = {10}
           style = {mapStyles}
           initialCenter = {{ lat: 38.889321, lng: -77.050166 }}
-        />
+        >
+        {this.displayMarkers()}
+        </Map>
       </div>
     );
   }
 }
-
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyDbqBL1-PmwoQqfn7uQWLQaK9hPJFJQ8Ew'
